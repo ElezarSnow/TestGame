e6e38bd3481ef100d7869d8ef4a41b3159c852dc
Dmitriy <69668851+qwewerqwewer1@users.noreply.github.com>
2023-01-30 22:39:09 +0300

    add Flip AGAIN

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,35 +2,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-  Vector2 V_pos;
-  Rigidbody2D comp_RGBD2D;
-  public float speed = 5;
-
-//   public GameObject gameObj = eulerAngles;
-
+  Vector2 v_pos;
+  Rigidbody2D rgbd_2d;
   private Animator animus;
+  public float speed = 5;
+  private int lastStep = 0;
+  //  0 - в первом кадре никто ничего не жал, поэтому ни A ни D не могли быть нажаты
+  // -1 - записывается когда последняя клавиша была A
+  //  1 - записывается когда последняя клавиша была D
+
   void Awake()
   {
-    V_pos = new Vector2();
-    comp_RGBD2D = GetComponent<Rigidbody2D>();
+    v_pos = new Vector2();
+    rgbd_2d = GetComponent<Rigidbody2D>();
     animus = GetComponent<Animator>();
   }
-
-
   void Update()
   {
-    V_pos.x = Input.GetAxisRaw("Horizontal") * speed;
-    V_pos.y = Input.GetAxisRaw("Vertical") * speed;
-    
-    comp_RGBD2D.velocity = V_pos;
+    v_pos.x = Input.GetAxisRaw("Horizontal") * speed;
+    v_pos.y = Input.GetAxisRaw("Vertical") * speed;
+    rgbd_2d.velocity = v_pos;
 
-    if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)) animus.SetBool("isRun", true);
+    if (Input.GetKeyDown(KeyCode.D))
+    {
+      if (lastStep == -1) this.transform.localScale = new Vector3(1, 1, 1);
+      animus.SetBool("isRun", true);
+      lastStep = 1;
+    }
+
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+      if (lastStep == 1) this.transform.localScale = new Vector3(-1, 1, 1);
+      animus.SetBool("isRun", true);
+      lastStep = -1;
+    }
+
     if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) animus.SetBool("isRun", false);
-
-    
   }
-
-//   void Rotate() { 
-    
-//   }
 }
